251c1ec07d6159dc4b489d8bedd6cc3dbda9299d
lukas0brunner <143714479+lukas0brunner@users.noreply.github.com>
2024-10-31 11:44:49 +0100

    made color theme

--- cardetailschecker/constants/Colors.ts
+++ cardetailschecker/constants/Colors.ts
@@ -3,17 +3,17 @@ const tintColorDark = '#fff';
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+    text: '#151712',
+    background: '#eff0eb',
+    primary: '#484d3d',
+    secondary: '#9eb3a4',
+    accent: '#627a70'
   },
   dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
+    text: '#ebede8',
+    background: '#13140f',
+    primary: '#bdc2b2',
+    secondary: '#4c6152',
+    accent: '#859d93'
   },
 };
