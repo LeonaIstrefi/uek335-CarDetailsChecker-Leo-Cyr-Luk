bb3a61ce670d1238075fa76932b25580d977c549
Gianluca Daffre <gianluca.daffre@noseryoung.ch>
2024-11-06 16:58:32 +0100

    fixed conditional rendering

--- cardetailschecker/components/NavBar.tsx
+++ cardetailschecker/components/NavBar.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
+import {useEffect, useState} from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createMaterialBottomTabNavigator} from 'react-native-paper/react-navigation';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useTheme } from "react-native-paper";
+import {useTheme} from "react-native-paper";
 import {Login} from "../tabs/Login";
 import {Register} from "../tabs/Register";
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function Homepage() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>Homepage!</Text>
         </View>
     );
@@ -20,7 +20,7 @@ function Homepage() {
 
 function Overview() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>Overview</Text>
         </View>
     );
@@ -28,7 +28,7 @@ function Overview() {
 
 function Profile() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>Profile</Text>
         </View>
     );
@@ -60,47 +60,42 @@ export default function Nav() {
 
     return (
         <NavigationContainer>
-            {isAuthenticated ? (
-                <Tab.Navigator
-                    initialRouteName={"Home"}
-                    activeColor={theme.colors.outline}
-                    inactiveColor={theme.colors.outline}
-                    barStyle={{ backgroundColor: theme.colors.secondary }}
-                >
-                    <Tab.Screen
-                        name="Home"
-                        component={Homepage}
-                        options={{
-                            tabBarIcon: ({ color }) => (
-                                <MaterialCommunityIcons name="home" color={color} size={24} />
-                            ),
-                        }}
-                    />
-                    <Tab.Screen
-                        name="Overview"
-                        component={Overview}
-                        options={{
-                            tabBarIcon: ({ color }) => (
-                                <MaterialCommunityIcons name="car" color={color} size={24} />
-                            ),
-                        }}
-                    />
-                    <Tab.Screen
-                        name="Profile"
-                        component={Profile}
-                        options={{
-                            tabBarIcon: ({ color }) => (
-                                <MaterialCommunityIcons name="account-outline" color={color} size={24} />
-                            ),
-                        }}
-                    />
-                </Tab.Navigator>
-            ) : (
-                <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={"Login"}>
-                    <Stack.Screen name="Login" component={Login} />
-                    <Stack.Screen name="Register" component={Register} />
-                </Stack.Navigator>
-            )}
+            <Tab.Navigator
+                initialRouteName={"Home"}
+                activeColor={theme.colors.outline}
+                inactiveColor={theme.colors.outline}
+                barStyle={{backgroundColor: theme.colors.secondary}}
+            >
+                <Tab.Screen
+                    name="Home"
+                    component={Homepage}
+                    options={{
+                        tabBarIcon: ({color}) => (
+                            <MaterialCommunityIcons name="home" color={color} size={24}/>
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Overview"
+                    component={Overview}
+                    options={{
+                        tabBarIcon: ({color}) => (
+                            <MaterialCommunityIcons name="car" color={color} size={24}/>
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={Profile}
+                    options={{
+                        tabBarIcon: ({color}) => (
+                            <MaterialCommunityIcons name="account-outline" color={color} size={24}/>
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+
+
         </NavigationContainer>
     );
 }
